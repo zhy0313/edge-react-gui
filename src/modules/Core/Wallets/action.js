30cb956965f8aaa38a37921c215b74a2c6cd3ee1
Kevin Sullivan <kevin@airbitz.co>
2018-03-14 16:18:30 -0700

    Load receive addresses on login

--- src/modules/Core/Wallets/action.js
+++ src/modules/Core/Wallets/action.js
@@ -1,5 +1,5 @@
 // @flow
-import type { EdgeCurrencyWallet } from 'edge-core-js'
+import type { EdgeCurrencyWallet, EdgeReceiveAddress } from 'edge-core-js'
 
 import type { Dispatch, GetState } from '../../ReduxTypes'
 import * as SETTINGS_SELECTORS from '../../UI/Settings/selectors'
@@ -8,12 +8,13 @@ import * as CORE_SELECTORS from '../selectors'
 export const PREFIX = 'Core/Wallets/'
 export const UPDATE_WALLETS = PREFIX + 'UPDATE_WALLETS'
 
-export const updateWallets = (activeWalletIds: Array<string>, archivedWalletIds: Array<string>, currencyWallets: { [id: string]: EdgeCurrencyWallet }) => ({
+export const updateWallets = (activeWalletIds: Array<string>, archivedWalletIds: Array<string>, currencyWallets: { [id: string]: EdgeCurrencyWallet }, receiveAddresses: { [id: string]: EdgeReceiveAddress }) => ({
   type: UPDATE_WALLETS,
   data: {
     activeWalletIds,
     archivedWalletIds,
-    currencyWallets
+    currencyWallets,
+    receiveAddresses
   }
 })
 
@@ -29,15 +30,35 @@ export const updateWalletsRequest = () => (dispatch: Dispatch, getState: GetStat
   const account = CORE_SELECTORS.getAccount(state)
   const { activeWalletIds, archivedWalletIds, currencyWallets } = account
 
-  for (const walletId: string of Object.keys(currencyWallets)) {
-    const edgeWallet: EdgeCurrencyWallet = currencyWallets[walletId]
-    if (edgeWallet.type === 'wallet:ethereum') {
-      if (state.ui.wallets && state.ui.wallets.byId && state.ui.wallets.byId[walletId]) {
-        const enabledTokens = state.ui.wallets.byId[walletId].enabledTokens
-        edgeWallet.enableTokens(enabledTokens)
+  getReceiveAddresses(currencyWallets).then((receiveAddresses) => {
+    for (const walletId: string of Object.keys(currencyWallets)) {
+      const edgeWallet: EdgeCurrencyWallet = currencyWallets[walletId]
+      if (edgeWallet.type === 'wallet:ethereum') {
+        if (state.ui.wallets && state.ui.wallets.byId && state.ui.wallets.byId[walletId]) {
+          const enabledTokens = state.ui.wallets.byId[walletId].enabledTokens
+          edgeWallet.enableTokens(enabledTokens)
+        }
       }
     }
-  }
 
-  return dispatch(updateWallets(activeWalletIds, archivedWalletIds, currencyWallets))
+    return dispatch(updateWallets(activeWalletIds, archivedWalletIds, currencyWallets, receiveAddresses))
+  })
+}
+
+const getReceiveAddresses = (currencyWallets: { [id: string]: EdgeCurrencyWallet }): Promise<{ [id: string]: EdgeReceiveAddress }> => {
+  const ids = Object.keys(currencyWallets)
+  const promises = ids.map(id => {
+    return currencyWallets[id].getReceiveAddress()
+  })
+  return Promise.all(promises).then(receiveAddresses => {
+    return ids.reduce(
+      (result, id, index) => {
+        return {
+          ...result,
+          [id]: receiveAddresses[index]
+        }
+      },
+      {}
+    )
+  })
 }
